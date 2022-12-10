cec8ae5b71e40ea08150616e94550257d5e5149e
Meruyert <kaliaskarova.mika@gmail.com>
2022-12-10 19:17:04 +0600

    task10

--- 10/Program.cs
+++ 10/Program.cs
@@ -2,7 +2,7 @@
 
 Console.Clear();
 
-Console.Write("Введи трёхзначное число: ");
+Console.Write("Введите трехзначное число: ");
 int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
 string stringNumber = Convert.ToString(threeDigitNumber);
 Console.WriteLine("Вторая цифра числа - "+stringNumber[1]);

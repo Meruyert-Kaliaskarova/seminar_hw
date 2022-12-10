8798d79c62d726306458790139cdabb63b48b7ec
Meruyert <kaliaskarova.mika@gmail.com>
2022-12-10 19:29:27 +0600

    Задача 13

---
+++ 13/Program.cs
@@ -0,0 +1,13 @@
+﻿// task 13
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string any_number = Convert.ToString(number);
+if (any_number.Length > 2){
+  Console.WriteLine("Третья цифра - " + any_number[2]);
+}
+else {
+  Console.WriteLine("- Третьей цифры нет");
+}

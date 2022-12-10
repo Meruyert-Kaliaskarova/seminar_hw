831259e7033f303c1e87b23ae797dd76b700252a
Meruyert <kaliaskarova.mika@gmail.com>
2022-12-10 19:41:21 +0600

    Семинар 2 Задача 10

---
+++ S2_task 10/Program.cs
@@ -0,0 +1,8 @@
+﻿// task 10
+
+Console.Clear();
+
+Console.Write("Введите трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("Вторая цифра числа - "+stringNumber[1]);

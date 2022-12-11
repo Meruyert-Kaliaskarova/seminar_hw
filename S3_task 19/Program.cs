f81d7b4c99bf264fa5ed0633332615453ed1fefe
Meruyert <kaliaskarova.mika@gmail.com>
2022-12-11 16:29:57 +0600

    Семинар 3 Задача 19

--- S3_task 19/Program.cs
+++ S3_task 19/Program.cs
@@ -6,7 +6,7 @@ Console.Write("Введите число: ");
 string? number = Console.ReadLine();
 
 void CheckingNumber(string number){
-  if (number[0]==number[4] || number[1]==number[3]){
+  if (number[0]==number[4] && number[1]==number[3]){
     Console.WriteLine($"Число {number} - палиндром.");
   }
   else Console.WriteLine($"Число {number} - не палиндром.");

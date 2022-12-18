197f99fba9a89a82c6557089751dc814a7f9de79
Meruyert <kaliaskarova.mika@gmail.com>
2022-12-18 17:43:41 +0600

    Seminar 4 Task 27

---
+++ S4_task 27/Program.cs
@@ -0,0 +1,12 @@
+﻿// task 27
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine()), sum = 0;
+while (num > 0)
+{
+    sum = sum + num % 10;
+    num /= 10;
+}
+Console.WriteLine($"Сумма цифр в чсиле - {sum}");
